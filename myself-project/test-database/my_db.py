3aaa661186f588d8c05b0005cf86fabe63114165
rockwang465 <1031653788@qq.com>
2019-03-27 23:27:33 +0800

    update db.py file

--- myself-project/test-database/my_db.py
+++ myself-project/test-database/my_db.py
@@ -5,9 +5,11 @@ import os
 import sys
 
 db_file_name = "db_file.txt"
-db_file_path = '/var/mobile/Containers/Data/Application/1A91F6C8-EA17-43F5-A3EA-C42F498E529B/Documents/KeepData/files/db_file.txt'
-# db_file_path = 'db_file.txt'
-notice_format = 'delete/select [feild] from [table_name] where [field] >/=/</like [condition]\nadd [table_name] ("id","name","age","job","phone")\nupdate [table_name] set [field]="value1" where [feild]="value" '
+# db_file_path = '/var/mobile/Containers/Data/Application/1A91F6C8-EA17-43F5-A3EA-C42F498E529B/Documents/KeepData/files/db_file.txt'
+db_file_path = 'db_file.txt'
+notice_format = 'delete/select [feild] from [table_name] where [field] >/=/</like [condition]\n' \
+                'add [table_name] ("id","name","age","job","phone")\n' \
+                'update [table_name] set [field]="value1" where [feild]="value" '
 table_name = "db_info"
 
 
@@ -20,34 +22,50 @@ table_name = "db_info"
 # def opera_select():
 #     pass
 
-def opera_add(sql):
+
+# 下面4个opera_xxx函数为 增删改查函数。
+def opera_add(sql, db_data_list):  # add db_info ("6","wyc","22","AS","13812345678")
     print("add : ", sql)
     if "(" not in sql or ")" not in sql:
         print("Error2 : syntax is error, please input : \n %s" % notice_format)
     else:
         add_head_sql, add_tail_sql = sql.split("(")
-        print(add_head_sql, add_tail_sql)
-        if add_head_sql[0].split() == 2 and (add_head_sql[0].split())[1] == table_name:
-            pass
+        # print(add_head_sql, add_tail_sql)
+        # print(len(add_head_sql.split()))
+        # print(add_head_sql.split()[1])
+        if len(add_head_sql.split()) == 2 and add_head_sql.split()[1] == table_name:
+            replace_value1 = add_tail_sql.replace(")", "")
+            replace_value2 = replace_value1.replace('"', '')
+            replace_value3 = replace_value2.split(",")
+            if len(replace_value3) != len(db_data_list):
+                # print(replace_value3)
+                print("Error : add field amount is error")
+            else:   # 这地方有报错，有时间处理
+                for i in len(replace_value3):
+                    print(db_data_list[i])
+                    print(replace_value3[i])
+                    # db_data_list[i].append(replace_value3[i])
+            print(db_data_list)
         else:
-            print("Error : syntax error")
+            print("Error3 : syntax is error, please input : \n %s" % notice_format)
 
 
-def opera_delete(sql):
+def opera_delete(sql, db_data_list):
     print("delete", sql)
     # if "from" not in sql
 
 
-def opera_update(sql):
+def opera_update(sql, db_data_list):
     print("update", sql)
 
 
-def opera_select(sql):
+def opera_select(sql, db_data_list):
     print("select", sql)
 
 
+# 此函数为用户输入的内容，进行判断，并且分发给对应的函数(增删改查)。
 def get_input(db_data_list):
-    # notice_format = "add/delete/update/select [feild] from [table_name] where [field] >/=/</like [condition]"
+    # notice_format = "add/delete/update/select [field] from [table_name] where [field] >/=/</like [condition]"
     first_method = {
         'add': opera_add,
         'delete': opera_delete,
@@ -57,18 +75,18 @@ def get_input(db_data_list):
 
     while True:
         sql = input("Please input sql >>>")
+        if not sql: continue  # 注意，必须在input下面一行才可以。
         sql_head = sql.split()[0]
-        if not sql:
-            continue
-        elif sql_head not in ["add", "delete", "update", "select"] or len(sql.split()) < 3:
-            print("Error1 : syntax is error, please input : %s" % notice_format)
+        if sql_head not in ["add", "delete", "update", "select"] or len(sql.split()) < 3:
+            print("Error1 : syntax is error, please input :\n %s" % notice_format)
         else:
             for method in first_method:
                 if sql_head in method:
                     # print(sql_head, method)
-                    first_method[method](sql)
+                    first_method[method](sql, db_data_list)
 
 
+# 打开db_file.txt文件，并转换为数据列表：db_data_list
 def get_db_file():
     # get db_file.txt data
     if os.path.exists(db_file_path):
