1e5ddb4172222f8f5ab90de15c9923f082a96c74
rockwang465 <1031653788@qq.com>
2019-04-15 12:58:11 +0800

    Update study dir

---
+++ myself-project/study_dir/python_study/140.logging_module.py
@@ -0,0 +1,6 @@
+import logging
+
+logging.debug("This message is debug")
+logging.info("This message is info")
+logging.warning("This message is warning")
+logging.critical("This message is critical")
