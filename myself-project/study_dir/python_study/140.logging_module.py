751dd79fb1ae806a9b8d2ea333160164cf13aab5
rockwang465 <1031653788@qq.com>
2019-04-15 19:13:55 +0800

    Update image_info

--- myself-project/study_dir/python_study/140.logging_module.py
+++ myself-project/study_dir/python_study/140.logging_module.py
@@ -1,6 +1,20 @@
+# _*_ coding:utf-8 _*_
+
 import logging
+
+logging.basicConfig(filename="log-test.log",
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s : %(filename)s:%(funcName)s:%(lineno)d : %(message)s',
+                    datefmt='%Y-%m-%d %I:%M:%S %p')
+
+
+def say_err():
+    logging.error("There are has error")
+
+
+say_err()
 
 logging.debug("This message is debug")
 logging.info("This message is info")
 logging.warning("This message is warning")
-logging.critical("This message is critical")
+logging.critical("This message is critical\n")
